7d1db5fe74f08d55fe58a2b991bcf3c7294a5167
SinghAmandeep <amandeepsingh3095@gmail.com>
2024-01-16 16:46:54 +0100

    add img path

--- src/store.js
+++ src/store.js
@@ -3,5 +3,6 @@ import { reactive } from 'vue'
 
 export const store = reactive({
     projects: [],
+    imgPath: 'http://127.0.0.1:8000/storage/',
     apiProject: 'http://127.0.0.1:8000/api/projects',
 })
